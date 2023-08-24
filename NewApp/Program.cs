2d0d78554fc2374aea0ee3cdf4800a5728c53dac
tuongit368 <tuongit368@gmail.com>
2023-08-24 19:49:42 +0700

    update1

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Threading.Tasks.Dataflow;
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+// Vidu 5_c# 
+// Console.WriteLine(6);
+// int a = 9;
+// Console.WriteLine("a = " + a);
+
+// int a = 7;
+// int b = 8;
+// int c = a + b;
+
+// Console.WriteLine("a = {0} ",a);
+// Console.WriteLine("b = {0} ",b);
+// Console.WriteLine("a + b = {0}  ",c);
+
+// internal class Program
+// {
+//     public static void Main(string[] args)
+//     {
+//         Console.WriteLine("Nhap du lieu tu ban phim : ");
+//         string str = Console.ReadLine();
+//         Console.WriteLine("Du lieu vua moi nhap vao la : " + str);
+//     }
+// }
+
+internal class Program
+{
+    public static void Main (string[] args)
+    {
+       string name ="Dang Ngoc Tuong";
+       int age = 20;
+
+       Console.WriteLine ("Ten : {0}, Tuoi : {1}",name, age);
+    }
+}
