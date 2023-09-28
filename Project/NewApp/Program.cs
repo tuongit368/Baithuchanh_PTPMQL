97c75a3262fd55e524559a1eb1f56dbc8d3c99fd
tuongit368 <tuongit368@gmail.com>
2023-09-28 17:55:57 +0700

    Array

--- Project/NewApp/Program.cs
+++ Project/NewApp/Program.cs
@@ -1,4 +1,34 @@
-﻿using NewApp.Models;
-Employee epl = new Employee();
-epl.Input();
-epl.Output();
+﻿using System.Runtime.ConstrainedExecution;
+using System.Dynamic;
+using System;
+using NewApp.Models;
+// khai bao kich thuoc cua array
+int n;
+do {
+    try{
+        System.Console.Write("n = ");
+        n = Convert.ToInt32(Console.ReadLine());
+
+    }
+    catch {
+        n = 0;
+    }
+}while (n<1);
+
+// Khoi tao array
+// KDL[] name = new KDL [KT];
+int [] A = new int[n];
+// Add cac phan tu vao array
+// gan gia tri cho phan tu thu 2
+for(int i = 0; i < A.Length; i++ )
+{
+    System.Console.Write("A[{0}] = ", i);
+    A[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+// HIen thi cac phan tu trong array
+System.Console.WriteLine("------------------------------");
+for(int i = 0; i < A.Length; i++)
+{
+    System.Console.Write("{0}\t",A[i]);
+}
