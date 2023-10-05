80bd2cfdf8952436a93fb6093c9e663dd7854bb7
tuongit368 <tuongit368@gmail.com>
2023-10-05 19:53:50 +0700

    Tao StudentController

--- DemoMVC/Controllers/StudentController.cs
+++ DemoMVC/Controllers/StudentController.cs
@@ -1,14 +1,17 @@
-// using Microsoft.AspNetCore.Mvc;
+// Ho Va Ten : Dang Ngoc Tuong  || Ma Sinh Vien : 2121050140
 
-// namespace DemoMVC.Controllers
-// {
-//     public class StudentController : Controller
-//     {
-//         public IActionResult Index()
-//         {
-//             return View();
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoMVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
             
-//         }
+        }
 
-//     }
-// }
+    }
+}
