c077c4a380ba7536c645c39db278485bd14625ad
tuongit368 <tuongit368@gmail.com>
2023-10-05 19:51:27 +0700

    Update DemoMVC

--- DemoMVC/Controllers/DemoController.cs
+++ DemoMVC/Controllers/DemoController.cs
@@ -1,23 +1,23 @@
-// using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 
-// namespace DemoMVC.Controllers
-// {
-//     public class DemoController : Controller
-//     {
-//         // Khai bao cac phuong thuc 
-//         public IActionResult Index()
-//         {
-//             return View();
-//         }
+namespace DemoMVC.Controllers
+{
+    public class DemoController : Controller
+    {
+        // Khai bao cac phuong thuc 
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-//         //nhan du lieu tu view gui len ( Bat buoc )
-//         [HttpPost]
-//         public IActionResult Index(string fname, string Email)
-//         {
+        //nhan du lieu tu view gui len ( Bat buoc )
+        [HttpPost]
+        public IActionResult Index(string fname, string Email)
+        {
 
-//             string thongbao = "FullName: " + fname + " || " + " Email:" + Email;
-//             ViewBag.ThongBao = thongbao; 
-//             return View();
-//         }
-//     }
-// }
+            string thongbao = "FullName: " + fname + " || " + " Email:" + Email;
+            ViewBag.ThongBao = thongbao; 
+            return View();
+        }
+    }
+}
