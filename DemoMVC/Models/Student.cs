a8cfe3a10f41e1529d807032fac11b91e70276c9
tuongit368 <tuongit368@gmail.com>
2023-10-05 20:14:04 +0700

    add file Student.cs

---
+++ DemoMVC/Models/Student.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System;
+namespace NewApp.Models
+{
+    public class Student
+    {
+        public string StudentID { get; set; }   
+        public string FullName  { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+
+        public void Input()
+        {
+            try{
+                System.Console.Write("Student ID : ");
+                StudentID = Console.ReadLine();
+            }catch(Exception ex)
+            {
+                System.Console.WriteLine("Co ngoai le xay ra!");
+            }
+            
+            System.Console.Write("FullName : ");
+            FullName = Console.ReadLine();
+            
+            System.Console.Write("Address : ");
+            Address = Console.ReadLine();
+            
+            try{
+                System.Console.Write("Age : ");
+                Age = Convert.ToInt32(Console.ReadLine());
+            }catch(Exception ex)
+            {
+                System.Console.WriteLine("co ngoai le xay ra !");
+            }
+            
+        }
+
+        public void Output()
+        {
+            System.Console.WriteLine("StudentID: "+ StudentID +" - "+"FullName: "+ FullName +" - "+"Address: "+ Address +" - "+"Age: "+ Age +"Tuoi");
+        }
+
+        
+    }
+}
