af392835e3c0e96644823b5836814285b440e52d
tuongit368 <tuongit368@gmail.com>
2023-10-07 22:27:08 +0700

    Add EmployeeController.cs

---
+++ DemoMVC/Controllers/EmployeeController.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.Design;
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ DemoMVC/Models/Employee.cs
@@ -0,0 +1,44 @@
+
+namespace NewApp.Models
+{
+    public class Employee
+    {
+        public string MaNhanVien { get; set; } 
+        public string TenNhanVien { get; set; } 
+        public int Tuoi { get; set; }   
+        public int Luong { get; set; }  
+
+        public void Input()
+        {
+            try{
+                System.Console.Write("Ma Nhan Vien : ");
+                MaNhanVien = Console.ReadLine();
+            }catch(Exception ex)
+            {
+                System.Console.WriteLine("Co ngoai le xay ra");
+            }
+            
+            System.Console.Write("Ten Nhan Vien : ");
+            TenNhanVien = Console.ReadLine();
+            
+        try{
+            System.Console.Write("Tuoi : ");
+            Tuoi = Convert.ToInt32(Console.ReadLine());
+            
+            System.Console.Write("Luong : ");
+            Luong = Convert.ToInt32(Console.ReadLine());
+        }catch(Exception ex)
+        {
+            System.Console.WriteLine("Co ngoai le xay ra");
+        }
+            
+        }
+
+        public void Output()
+        {
+            System.Console.WriteLine("--------------------------");
+            System.Console.WriteLine(MaNhanVien + "-" + TenNhanVien + "-" + Tuoi + "-" + Luong + "USD");
+
+        }
+    }
+}
