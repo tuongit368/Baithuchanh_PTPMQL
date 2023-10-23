053606e9ccbb47c1c0e97f273f4fcf016674acc1
tuongit368 <tuongit368@gmail.com>
2023-10-23 20:42:29 +0700

    Add Class DaiLy in Models (Ex page20)

---
+++ DemoMVC/Models/DaiLy.cs
@@ -0,0 +1,11 @@
+namespace DemoMVC.Models
+{
+    public class DaiLy : HeThongPhanPhoi
+    {
+        public string MaDaiLy { get; set; }
+        public string TenDaiLy { get; set; }
+        public string DiaChi { get; set; }
+        public string NguoiDaiDien { get; set; }
+        public string DienThoai { get; set; }
+    }
+}
