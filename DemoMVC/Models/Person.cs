6039f88a2729e7396676fcf8193ca3ac2d2ebfcb
tuongit368 <tuongit368@gmail.com>
2023-10-23 20:35:41 +0700

    Add Class Person in Models (Ex page20)

---
+++ DemoMVC/Models/Person.cs
@@ -0,0 +1,10 @@
+namespace DemoMVC.Models
+{
+    public class Person 
+    {
+        // Thuoc tinh
+        public string PersonId { get; set; }
+        public string FullName { get; set; }
+        public string Address { get; set; }
+    }
+}
